cdc84a57960672246fa7453998a4600e03b7c455
Daniel <welxeorwel@gmail.com>
2022-10-29 22:10:02 +0300

    wip

---
+++ db.js
@@ -0,0 +1,13 @@
+class Database{
+    initialize(){
+        this.cats = []; 
+    }
+    async addCat(cat){
+
+    }
+    async getCats(){
+        return this.cats;
+    }
+}
+
+module.exports = Database;
